7779d18fe5b1d49d39ef67e3c5b30bed4772c697
bakikucukcakiroglu <baki.kucukcakiroglu@gmail.com>
2024-05-25 16:32:46 +0200

    save search history

--- fe/src/components/GoogleSearch.tsx
+++ fe/src/components/GoogleSearch.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import { CssBaseline, Box, Container, Input, Button, Typography } from '@mui/joy';
-import Sidebar from './sidebar';
+import React, { useEffect } from 'react';
+import { CssBaseline, Box, Container} from '@mui/joy';
+import Sidebar from './Sidebar';
 
 function GoogleSearch() {
-  return (
-    <>
-      <CssBaseline />
-      <Sidebar />
 
-      <Container maxWidth="lg" sx={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent:'center'}}>
-        <Box sx={{ flex: 2, textAlign: 'center', maxWidth: '500px' }}>
-          <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google" style={{ width: '50%' }} />
-          <div className="gcse-search"></div>
-        </Box>
-      </Container>
-      <Sidebar />
+    const [searchHistory, setSearchHistory] = React.useState<string[]>([]);
 
-    </>
-  );
+    useEffect(() => {
+        const inputElement =document.getElementsByName('search')[0] as HTMLInputElement;
+        const element = document.getElementsByClassName('gsc-search-button');
+        element[1]?.addEventListener('click', () => {
+            if (inputElement) {
+                console.log(inputElement.value);
+                setSearchHistory([...searchHistory, inputElement.value]);
+            }
+        });
+    }, [searchHistory]);
+
+    console.log(searchHistory);
+
+    return (
+        <>
+        <CssBaseline />
+        <Sidebar />
+        <Container maxWidth="lg" sx={{ display: 'flex', alignItems: 'center', justifyContent:'center'}}>
+            <Box sx={{ flex: 2, textAlign: 'center', maxWidth: '500px' }}>
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google" style={{ width: '50%' }} />
+            <div className="gcse-search"></div>
+            </Box>
+        </Container>
+        <Sidebar />
+        </>
+    );
 }
 
 export default GoogleSearch;
--- fe/src/components/header.tsx
+++ fe/src/components/header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../style/Header.css';
+import { Box, Typography } from '@mui/joy';
 
 function Header() {
   return (
     <header className="App-header">
-      <h1>AQA Ad Generator</h1>
+      <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
+        <Box display={'flex'} >
+          <img src="https://framerusercontent.com/images/SmVLY5TwJDtVXHzKjU3pWW7qqk.png"  alt="logo" width={30} height={30}  />
+          <Typography level="h4" component="div" sx={{ flexGrow: 1, marginLeft:1 }}>Zero Knowledge Ad Service</Typography>
+        </Box>
+      </Box>
     </header>
   );
 }
