e739b04851041d8c527d5932ba1973ef85d39617
bakikucukcakiroglu <baki.kucukcakiroglu@gmail.com>
2024-05-25 17:19:01 +0200

    element load problem fix

--- fe/src/components/GoogleSearch.tsx
+++ fe/src/components/GoogleSearch.tsx
@@ -1,37 +1,62 @@
-import React, { useEffect } from 'react';
-import { CssBaseline, Box, Container} from '@mui/joy';
-import Sidebar from './sidebar';
+import React, { useEffect, useState, useRef } from 'react';
+import { CssBaseline, Box, Container } from '@mui/joy';
+import Sidebar from './Sidebar';
 
 function GoogleSearch() {
+  const [searchHistory, setSearchHistory] = useState<string[]>([]);
+  const inputElementRef = useRef<HTMLInputElement | null>(null);
+  const searchButtonRef = useRef<HTMLElement | null>(null);
 
-    const [searchHistory, setSearchHistory] = React.useState<string[]>([]);
+  useEffect(() => {
+    const setupSearchListener = () => {
+      const parentElement = document.getElementById('gs_tti50');
+      const inputElement = parentElement?.querySelector('input[name="search"]') as HTMLInputElement;
+      const searchButton = document.querySelector('.gsc-search-button') as HTMLElement;
 
-    useEffect(() => {
-        const inputElement =document.getElementsByName('search')[0] as HTMLInputElement;
-        const element = document.getElementsByClassName('gsc-search-button');
-        element[1]?.addEventListener('click', () => {
-            if (inputElement) {
-                console.log(inputElement.value);
-                setSearchHistory([...searchHistory, inputElement.value]);
-            }
-        });
-    }, [searchHistory]);
+      if (inputElement && searchButton) {
 
+        console.log('Found search elements')
+        inputElementRef.current = inputElement;
+        searchButtonRef.current = searchButton;
+
+        const handleSearch = () => {
+          if (inputElementRef.current) {
+            setSearchHistory(prev => [...prev, inputElementRef.current!.value]);
+          }
+        };
+
+        searchButton.addEventListener('click', handleSearch);
+
+        // Cleanup event listener on unmount
+        return () => {
+          searchButton.removeEventListener('click', handleSearch);
+        };
+      } else {
+        setTimeout(setupSearchListener, 500); // Poll every 500ms until elements are found
+      }
+    };
+
+    setupSearchListener(); // Start polling
+
+  }, []);
+
+  useEffect(() => {
     console.log(searchHistory);
+  }, [searchHistory]);
 
-    return (
-        <>
-        <CssBaseline />
-        <Sidebar />
-        <Container maxWidth="lg" sx={{ display: 'flex', alignItems: 'center', justifyContent:'center'}}>
-            <Box sx={{ flex: 2, textAlign: 'center', maxWidth: '500px' }}>
-            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google" style={{ width: '50%' }} />
-            <div className="gcse-search"></div>
-            </Box>
-        </Container>
-        <Sidebar />
-        </>
-    );
+  return (
+    <>
+      <CssBaseline />
+      <Sidebar />
+      <Container maxWidth="lg" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Box sx={{ flex: 2, textAlign: 'center', maxWidth: '500px' }}>
+          <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google" style={{ width: '50%' }} />
+          <div id="gs_tti50" className="gcse-search"></div> {/* Add your known ID here */}
+        </Box>
+      </Container>
+      <Sidebar />
+    </>
+  );
 }
 
 export default GoogleSearch;
