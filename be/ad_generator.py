f219a04bd27c939f6b54f3c370df69b86482131e
bakikucukcakiroglu <baki.kucukcakiroglu@gmail.com>
2024-05-25 17:08:02 +0200

    search history fix

--- be/ad_generator.py
+++ be/ad_generator.py
@@ -1,42 +1,32 @@
 import torch
-import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 class AdGenerator:
     def __init__(self):
+        self.model_name = "meta-llama/Llama-2-7b-chat-hf"
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.pipeline = transformers.pipeline(
-            "text-generation",
-            model="meta-llama/Meta-Llama-3-8B-Instruct",
-            model_kwargs={"torch_dtype": torch.bfloat16},
-            device=self.device,
+        self.tokenizer  = AutoTokenizer.from_pretrained(self.model_name)
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_name,token= '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').to(self.device)
+
+    def generate_ads(self, search_data, max_length=256, num_return_sequences=2):
+        inputs = self.tokenizer(search_data, return_tensors="pt", padding=True)
+        
+        input_ids = inputs["input_ids"].to(self.device)
+        attention_mask = inputs["attention_mask"].to(self.device)
+
+        output = self.model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            max_length=max_length,
+            num_return_sequences=num_return_sequences,
+            no_repeat_ngram_size=2,
+            do_sample=True,
+            top_k=50,
+            top_p=0.95,
+            early_stopping=True,
         )
 
-    def generate_ads(self, search_data: str, max_length=256, num_return_sequences=2):
-        items = search_data.split("\n")
-        prefix = "I like things such as: "
-
-        joined_items = ", ".join(items)
-
-        suffix = "\n\n\nBased on the interests above, I will now let you know which option of the following I am most interested in."
-        suffix += "\n1. garden hose"
-        suffix += "\n2. basketball"
-        suffix += "\n3. swimming pool"
-        suffix += "\n4. macbook"
-        suffix += "\n5. beer"
-        suffix += "\n6. crypto swag"
-        suffix += "\nFrom the options 1-6, I'm most interested in number "
-        prompt = prefix + joined_items + suffix
-        print("prompt", prompt)
-        ads: list[str] = [
-            r["generated_text"]
-            for r in self.pipeline(
-                prompt,
-                max_new_tokens=1,
-                num_return_sequences=num_return_sequences,
-            )
-        ]
-        ads = [ad.removeprefix(prompt) for ad in ads]
-        print(ads)
-
+        ads = [self.tokenizer.decode(out, skip_special_tokens=True) for out in output]
         return ads
